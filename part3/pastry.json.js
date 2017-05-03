c81b7f03c242d03bcd9d98d240c616471631487c
abrahamlolita <abrahamlolita@gmail.com>
2017-05-03 11:57:23 -0700

    COMM - 644 - Assignment 10
    COMM - 644 - Assignment 10

---
+++ part3/pastry.json.js
@@ -0,0 +1,43 @@
+{"pastries":[
+	{
+		"name":"Apple Pie",
+        "description":"Apple pie is a fruit pie (or tart) in which the principal filling ingredient is apples. It is sometimes served with whipped cream, Cheddar cheese, or ice cream on top. Pastry is generally used top-and-bottom, making it a double-crust pie, the upper crust of which may be a circular shaped crust or a pastry lattice woven of strips; exceptions are deep-dish apple pie with a top crust only, and open-face Tarte Tatin.",
+        "source":"https://en.wikipedia.org/wiki/Apple_pie"
+	},
+	{
+		"name":"Baklava",
+		"description":"A Turkish pastry that is rich and sweet, made of layers of filo pastry filled with chopped nuts and sweetened with syrup or honey.",
+        "source":"https://en.wikipedia.org/wiki/Baklava"
+        
+	},
+    {
+		"name":"Cinnamon Roll",
+		"description":"A Cinnamon roll (also cinnamon bun, cinnamon swirl, cinnamon Danish and cinnamon snail) is a sweet roll served commonly in Northern Europe and North America. In North America its common use is for breakfast. Its main ingredients are flour, cinnamon, sugar, and butter, which provide a robust and sweet flavor. In some places it is eaten as a breakfast food and is often served with cream cheese or icing.",
+        "source":"https://en.wikipedia.org/wiki/Cinnamon_roll"
+	},
+    {
+		"name":"Danish pastry",
+		"description":"A sweet pastry, of Viennese origin, which has become a speciality of Denmark and neighboring Scandinavian countries. Called 'facturas' in Argentina and neighbouring countries.",
+        "source":"https://en.wikipedia.org/wiki/Danish_pastry"
+	},
+	{
+		"name":"Éclair",
+		"description":"An oblong pastry made with choux dough filled with a cream and topped with icing. The dough, which is the same as that used for profiterole, is typically piped into an oblong shape with a pastry bag and baked until it is crisp and hollow inside. Once cool, the pastry then is filled with a coffee or chocolate flavoured pastry cream (crème pâtissière), custard, whipped cream, or chiboust cream; and iced with fondant icing. The éclair probably originated in France during the nineteenth century.",
+        "source":"https://en.wikipedia.org/wiki/%C3%89clair"
+	},
+	{
+		"name":"Macaron",
+		"description":"These have a debated origin but the earliest form of modern macaron was likely baked in France in the 1800s. French macarons are made with a mixture of almond flour and confectioners' sugar which is folded into a meringue of stiffly beaten egg whites. This mixture is tinted with food coloring and baked into disks, which are sandwiched with buttercream, ganache, or curd. Known for its smooth skin, ruffled feet, and delicate texture.",
+        "source":"https://en.wikipedia.org/wiki/Macaron"
+	},
+    {
+		"name":"Mille-feuille",
+		"description":"The mille-feuille (thousand sheets) vanilla slice, cream slice, custard slice, also known as the Napoleon or kremschnitt, is a pastry originating in France. Traditionally, a mille-feuille is made up of three layers of puff pastry (pâte feuilletée), alternating with two layers of pastry cream (crème pâtissière), but sometimes whipped cream, or jam are substituted. The top pastry layer is dusted with confectioner's sugar, and sometimes cocoa, or pulverized nuts (e.g. roasted almonds). Alternatively the top is glazed with icing or fondant in alternating white (icing) and brown (chocolate) stripes, and combed.",
+        "source":"https://en.wikipedia.org/wiki/Mille-feuille"
+	},
+    {
+		"name":"Palmier",
+		"description":"Palmiers are a German, Spanish, French, Italian, Jewish, and Portuguese pastry (among other cuisines, like those of the former Spanish colonies in the Americas) formed in a palm or butterfly shape. Made using puff pastry, sugar and sometimes honey.",
+        "source":"https://en.wikipedia.org/wiki/Palmier"
+	}
+]}
